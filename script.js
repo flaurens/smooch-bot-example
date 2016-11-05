06304759c2e513579030bdb2691fc70317177b70
Felix Laurens <laurensf@gmail.com>
2016-11-05 15:04:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,8 @@ Is that OK? %[Yes](postback:yes) %[No](postback:no)`))
             return bot.getProp('name')
                 .then((name) => bot.say(`Sorry ${name}, my creator didn't ` +
                         'teach me how to do anything else!'))
-                .then(() => 'finish');
+                .then(() => 'finish'))
+                .then(() => 'How may I help you?');
         }
     }
 });
