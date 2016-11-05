a1cfd60dc52e9f944c1c4c346e92110afb88942d
Felix Laurens <laurensf@gmail.com>
2016-11-05 21:06:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ Is that OK? %[Yes](postback:yes) %[No](postback:no)`))
         receive: (bot, message) => {
             return bot.getProp('name')
                 .then((name) => bot.say(`Sorry ${name}, my creator didn't ` +
-                        'teach me how to do anything else!'))
+                        'teach me how to do anything else! I have to learn a lot'))
                 .then(() => 'finish'))
                 .then(() => 'How may I help you?');
         }
